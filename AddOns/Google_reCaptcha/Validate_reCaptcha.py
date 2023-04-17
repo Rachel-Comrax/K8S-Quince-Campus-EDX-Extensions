7d231652bf221ea70d3974b490a88b261247c3a9
Roy Melinek <roy.melinek@midlink.co.il>
2023-04-17 09:11:24 +0000

    feat: [CAMPUSIL-416] reCaptcha overrides

---
+++ AddOns/Google_reCaptcha/Validate_reCaptcha.py
@@ -0,0 +1,14 @@
+import requests
+from django.conf import settings
+from openedx.core.djangoapps.user_authn.exceptions import AuthFailedError
+from django.utils.translation import ugettext as _
+
+def validate_recaptcha(token):
+    response = requests.post('https://www.google.com/recaptcha/api/siteverify', data={
+        'secret': str(settings.GOOGLE_RECAPTCHA_SERVER_SIDE_KEY),
+        'response': token
+    })
+    response_dict = response.json()
+    if not response_dict['success']:
+        raise AuthFailedError ("The CAPTCHA you entered is incorrect. Please try again.")
+    return response.json()['success']
