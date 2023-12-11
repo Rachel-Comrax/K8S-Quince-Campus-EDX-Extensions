d73aa68636a54fdbce7d95fcc397bfd56f01f4b5
Eden Bahalul <edenb@campus.gov.il>
2023-12-11 14:00:03 +0000

    feat: [CAMPUSIL-1988] adding a class property that specifies whether the tab can be hidden for a particular course

--- campus_edx_extensions/courseware/tabs.py
+++ campus_edx_extensions/courseware/tabs.py
@@ -1,13 +1,6 @@
 from lms.djangoapps.courseware.tabs import DatesTab as DatesTabBase
 
-class DatesTab(DatesTabBase):
-    
-    @classmethod
-    def is_enabled(cls, course, user=None):
-        return True
-    
-    @classmethod
-    def is_hideable(cls, course, user=None):
-        return True
+class DatesTab(DatesTabBase):  
+    is_hideable =  True
     
 
