cec25e7ffe109e30e1e162e0047586c278458579
vladimirCampusGovIl <97379992+vladimirCampusGovIl@users.noreply.github.com>
2023-12-12 11:02:31 +0200

    Update courses_report.py
    removed unnecessary annotation

--- digital_gov_reports/courses_report.py
+++ digital_gov_reports/courses_report.py
@@ -7,32 +7,31 @@ from openedx.core.djangoapps.site_configuration import helpers as configuration_
 log = logging.getLogger(__name__)
 
 def get_digital_data_to_report(**data):
-#recive the outdata from data model
+    # recive the outdata from data model
     _queryset = CampusilReportableCoursesDigital.get_courses_to_report()
-#prepare the constant parametrs of certificare url
+    # prepare the constant parametrs of certificare url
     _domain = configuration_helpers.get_value('LMS_ROOT_URL', settings.LMS_ROOT_URL)
-    #_domain = _domain if _domain is not 'http://edx.devstack.lms:18000qwer1' else 'http://localhost:18000'  
     _prefix_certificate_url= f'{_domain}/certificates/'
-#Create an empty list to store the element values 
+    # create an empty list to store the element values 
     _output = []  
     _time_slot = data["DIGITAL_TIME_DELTA"]
     log.info(f'DIGITAL_TIME_DELTA={_time_slot}')
     
     for row in _queryset:
-#Adjust model properties to match product requirements
+        # adjust model properties to match product requirements
         course_id_str = str(row["_course_id"])
         course_end_strftime= None if row['course_end_date'] is None else row['course_end_date'].strftime("%Y-%m-%d")
-# return 1 if student pass the course else 0 
+        # return 1 if student pass the course else 0 
         latter_grade =  1 if row["course_latter_grade"] == "Pass" else 0 
-# the final grade in range of 0-100
+        # the final grade in range of 0-100
         total_grade = None if row["course_grade_total"] is None else row["course_grade_total"] * 100   
-# 'not attempt' means if the student has not started the course yet
+        # 'not attempt' means if the student has not started the course yet
         certificate_status = row['student_certificate_status']
-#certificare url for download: 
+        #certificare url for download: 
         verify_uuid = row['student_certificate_verify_uuid'] 
         certificate_url = None if verify_uuid is None else _prefix_certificate_url + verify_uuid
      
-# set the User info
+        # set the User info
         student = {
             "id": row["_student_id"],
             "first_name": row["student_first_name"],
@@ -44,7 +43,7 @@ def get_digital_data_to_report(**data):
             "certificate_url": certificate_url    
         }
         
-# set the output JSON with joined multiple tables data
+        # set the output JSON with joined multiple tables data
         fields_value = {
             "course_id": course_id_str,
             "report_do_Digital": row["reportable_course"],
