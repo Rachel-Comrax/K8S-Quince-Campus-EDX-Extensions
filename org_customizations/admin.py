c0b251758218ba85cd5815f7d009b0a4e14bd5fc
Eden Bahalul <edenb@campus.gov.il>
2024-02-05 09:33:16 +0000

    feat [CAMPUSIL-2297] Initial implementation of Django admin panel view for the Organization customizations model's

--- org_customizations/admin.py
+++ org_customizations/admin.py
@@ -1,14 +1,22 @@
 from django.contrib import admin
-from .models import OrganizationVerticals, Verticals
+from .models import OrganizationExtraData, OrganizationVerticals, Verticals
 
 class OrganizationVertical_Admin(admin.ModelAdmin): 
-    pass
-
+    list_display = ('display_name',)
+      
+    def display_name(self, obj):
+        return f"{obj.org.name} - {obj.vertical.name}"
+    
+    display_name.short_description = 'Organization - Vertical'
 
 # Vertical Admin
 class VerticalAdmin(admin.ModelAdmin):
     list_display = ('name',)  
     search_fields = ('name',)  
+    
+class OrganizationExtraDataAdmin(admin.ModelAdmin):
+    pass
 
 admin.site.register(Verticals, VerticalAdmin)
 admin.site.register(OrganizationVerticals, OrganizationVertical_Admin)
+admin.site.register(OrganizationExtraData, OrganizationExtraDataAdmin)
