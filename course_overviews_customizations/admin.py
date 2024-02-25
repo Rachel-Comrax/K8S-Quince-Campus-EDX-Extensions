f2318e0af47cf9429dee9b74134b808cf94f728b
Eden Bahalul <edenb@campus.gov.il>
2024-02-25 08:13:32 +0000

    feat: [CAMPUSIL-2293] changed the method name to align with the language convention

--- course_overviews_customizations/admin.py
+++ course_overviews_customizations/admin.py
@@ -1,34 +1,25 @@
 
-from openedx.core.lib.courses import clean_course_id
 
 from django.contrib import admin
-from django import forms
 from .models import  CourseOverviewExtraData
 
 class CourseOverviewExtraDataAdmin(admin.ModelAdmin): 
     
-    list_display = ('course_id','course_display_name', 'origins')
+    list_display = ('course__id','course__display_name', 'origin__name',)
     search_fields = ('course__display_name', 'origin__name')
-    raw_id_fields = ('course',)
-    
-    
-    def course_display_name(self, obj):
-        return obj.course.display_name
-    course_display_name.short_description = 'Course Display Name'
-    
-    def course_id(self, obj):
-        return obj.course.id
-    course_id.short_description = 'Course ID'
+    raw_id_fields = ('course', )
 
-    def origins (self, obj):
+    def course__display_name(self, obj):
+        return obj.course.display_name
+    course__display_name.short_description = 'Course Display Name'
+    
+    def course__id(self, obj):
+        return obj.course.id
+    course__id.short_description = 'Course ID'
+
+    def origin__name(self, obj):
     # returns all the related origins object associated with the current courseoverview instance 
         return ' , '.join( o.name for o in obj.origin.all())
-    origins.short_description = "course series"
-
-    def clean_course_id(self):
-        """
-        Validate the course id
-        """
-        return clean_course_id(self)
+    origin__name.short_description = "course series"
 
 admin.site.register(CourseOverviewExtraData, CourseOverviewExtraDataAdmin)
